fd318d8bab5e3dc3ba4ddea7205212e09d332dff
dbosle <32545222+dbosle@users.noreply.github.com>
2020-02-05 16:48:54 +0300

    Create stringToTurkishUpperLower.js File

---
+++ stringToTurkishUpperLower.js
@@ -0,0 +1,47 @@
+String.prototype.turkishToUpper = function () {
+    // -> Just for one word
+    // -> "çağla çağla" -> "ÇAĞLA ÇAĞLA"
+
+    var string = this;
+    var letters = {
+        "i": "İ",
+        "ş": "Ş",
+        "ğ": "Ğ",
+        "ü": "Ü",
+        "ö": "Ö",
+        "ç": "Ç",
+        "ı": "I"
+    };
+    string = string.replace(/(([iışğüçö]))/g, function (letter) {
+        return letters[letter];
+    })
+    return string.toUpperCase();
+}
+
+String.prototype.turkishToLower = function () {
+    // -> Just for one word
+    // -> "çağla ÇAĞLA ÇAğla" -> "çağla çağla çağla"
+
+    var string = this;
+    var letters = {
+        "İ": "i",
+        "I": "ı",
+        "Ş": "ş",
+        "Ğ": "ğ",
+        "Ü": "ü",
+        "Ö": "ö",
+        "Ç": "ç"
+    };
+    string = string.replace(/(([İIŞĞÜÇÖ]))/g, function (letter) {
+        return letters[letter];
+    })
+    return string.toLowerCase();
+}
+
+
+String.prototype.turkishCapitalize = function () {
+    // -> All words in a sentence
+    // -> "çağla çağla" -> "Çağla Çağla"
+
+    return this.split(" ").map(function (item) { return item[0].turkishToUpper() + item.substring(1, item.lenght) }).join(" ");;
+}
